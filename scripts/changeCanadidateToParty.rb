c79f04da767c20ca50d9da0c5a105a90aff53c9e
Julien Lamarche <julien.lamarche@gmail.com>
2018-05-21 12:30:30 -0400

    Strategy for multi mach

--- scripts/changeCanadidateToParty.rb
+++ scripts/changeCanadidateToParty.rb
@@ -44,8 +44,9 @@ if checkHeaders(affiliationPath, requiredHeadersAffiliation)
 end
 
 
-### Build a lookup table for names
+### Build a lookup table by name
 lastNameLookups = {}
+#ridingPartyLookups = {}
 CSV.open(affiliationPath, headers: :first_row) do |csv|
     csv.each do |row|
         nameParts = row["Candidate"].strip.split(' ')
@@ -53,12 +54,24 @@ CSV.open(affiliationPath, headers: :first_row) do |csv|
         exceptFirstName = nil
         exceptFirstName = nameParts.drop(1).join(' ').downcase if nameParts.count > 2
 
-
         [lastName, exceptFirstName].each {|nameLike|
             next if nameLike.nil?
             lastNameLookups[nameLike] = [] if lastNameLookups[nameLike].nil?
             lastNameLookups[nameLike] << row
         }
+
+        #riding = row["Riding"]
+        #party = row["Party"]
+        #ridingLookups[riding] = {} if ridingLookups[riding].nil?
+
+        # It does not exist
+        #if ridingLookups[riding][party].nil?
+        #    ridingLookups[riding][party] = row
+        #else # Special case, most likely 2 independants
+        #    othCount =  ridingLookups[riding][party].count
+        #    ridingLookups[riding][party + (othCount + 1).to_s] = row
+        #end
+
 
 
     end
@@ -79,23 +92,51 @@ Dir.foreach(resultsDir) do |csvFile|
         csv.each_with_index do |row, i|
             next if i > 0
 
+            thisFileMultiMatches = []
+            thisRidingIs = nil
+
             # Find the candidate headers
             candidates = row.headers[3..15].select {|h| h.present? }
 
             # Figure out their affilation
             candidateAffiliation = {}
-            candidates.each {|c|
+            candidates.each_with_index {|c, j|
                 lookup = c.downcase
                 case (lastNameLookups[lookup] or []).count
                     when 1
                         candidateAffiliation[c] = lastNameLookups[lookup][0]["Party"]
+                        thisRidingIs = lastNameLookups[lookup][0]["Riding"]
                         exactMatches.push(c)
+
+                        # If the riding was identified not on the first candidate, thisFileMultiMatches will have some unmatched candidates. Go through them.
+                        # There is very likely a smarter way to do this: iterate through the candidates first to find a single match, then go through all of them
+                        thisFileMultiMatches.each { |mm|
+                            smartPerRidingMatch = lastNameLookups[mm.downcase].select {|m| m["Riding"] == thisRidingIs}
+                            candidateAffiliation[mm] = smartPerRidingMatch[0]["Party"]
+                            exactMatches.push(mm)
+                            thisFileMultiMatches.delete(mm)
+                        }
+
+
                     when 0
                         noMatches.push(c)
                     else
-                        multiMatches.push(c)
+                        #byebug
+                        if not thisRidingIs.nil?
+                            smartPerRidingMatch = lastNameLookups[lookup].select {|m| m["Riding"] == thisRidingIs}
+                            if smartPerRidingMatch.count == 1
+                                candidateAffiliation[c] = smartPerRidingMatch[0]["Party"]
+                                exactMatches.push(c)
+                            else
+                                multiMatches.push(c)
+                            end
+                        else
+                            thisFileMultiMatches.push(c)
+                        end
                 end
             }
+
+            multiMatches += thisFileMultiMatches
 
             # Create a new polling area object
 
